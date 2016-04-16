0200d090f3205acc643d5e302fcc8a2c08367cb5
David Wong <dhyhwong@gmail.com>
2016-04-15 23:23:10 -0400

    boilerplate code and testing rig for bar chart widget

--- bar-chart.js
+++ bar-chart.js
@@ -1,5 +1,25 @@
 (function() {
   if (window.CTSWidgets) {
+
+    var BarChart = function(containerNode, dataNode, optionsNode) {
+      var barChart = Object.create(BarChart.prototype);
+
+      console.log('initialized a bar chart');
+
+      barChart.parseHTML = function() {
+
+      }
+
+      barChart.render = function() {
+
+      }
+
+      return barChart;
+    }
+
+    var defaultName = "bar-chart";
+
+    CTSWidgets[defaultName] = BarChart;
   }
   else {
     throw "CTS Widget Manager not found."
