6f3c07e5384fff80e4a5cfea36be55765ed8f02e
David Wong <dhyhwong@gmail.com>
2016-04-17 16:17:45 -0400

    finished bar chart widget code version 1.0

--- bar-chart.js
+++ bar-chart.js
@@ -1,24 +1,108 @@
 (function() {
   if (window.CTSWidgets) {
-
+    // bar chart constructor
     var BarChart = function(containerNode, dataNode, optionsNode) {
       var barChart = Object.create(BarChart.prototype);
 
-      console.log('initialized a bar chart');
+      // parse the data from the dataNode
+      barChart.parseData = function(dataNode) {
+        var data = [];
+        var rows = Array.from(dataNode.getElementsByClassName("row"));
+        rows.forEach(function(row) {
+          var values = row.getElementsByTagName("span");
+          data.push({
+            "key": values[0].textContent,
+            "value": parseFloat(values[1].textContent)
+          });
+        });
 
-      barChart.parseHTML = function() {
-
+        return data;
       }
 
-      barChart.render = function() {
+      // parse the options from the optionsNode
+      barChart.parseOptions = function(optionsNode) {
+        var options = {};
 
+        var yLabelNodes = optionsNode.getElementsByClassName("y-label");
+        var yLabel = yLabelNodes.length > 0 ? yLabelNodes[0].textContent : "Y-axis";
+        options["yLabel"] = yLabel;
+        
+        return options
       }
 
-      return barChart;
+      // render the widget in the container
+      barChart.render = function(container, data, options) {
+        var container = d3.select(container);
+        var yLabel = options["yLabel"];
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10, "%");
+
+        var svg = container.append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain(data.map(function(d) { return d.key; }));
+        y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text(yLabel);
+
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.key); })
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(d.value); })
+            .attr("height", function(d) { return height - y(d.value); });
+
+        function type(d) {
+          d.value = +d.value;
+          return d;
+        }
+      }
+
+      // initialization script
+      var container = containerNode;
+      var data = barChart.parseData(dataNode);
+      var options = barChart.parseOptions(optionsNode);
+
+      barChart.render(container, data, options);
     }
 
+    // attach the constructor to CTSWidgets
     var defaultName = "bar-chart";
-
     CTSWidgets[defaultName] = BarChart;
   }
   else {
