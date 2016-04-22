3da606a340e0dc3e840c8a278b6572fdb56bd73b
David Wong <dhyhwong@gmail.com>
2016-04-22 01:35:54 -0400

    moved files into different directories

--- widgets/bar-chart/bar-chart.js
+++ widgets/bar-chart/bar-chart.js
@@ -51,7 +51,7 @@
         }
 
         // import this widget's CSS into the shadow DOM
-        shadow.innerHTML = "<style>@import './bar-chart.css';</style>";
+        shadow.innerHTML = "<style>@import '../widgets/bar-chart/bar-chart.css';</style>";
 
         var shadowContainer = d3.select(shadow);
 
